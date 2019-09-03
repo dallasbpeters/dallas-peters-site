5507ccb6d9ddf7f676c0122f45526062566e943f
Dallas <dpeters@americanbible.org>
2019-09-03 13:21:52 -0500

    removes prefix for site domain

--- siteConfig.js
+++ siteConfig.js
@@ -2,7 +2,7 @@ module.exports = {
   name: "Dallas Peters",
   shortName: "Dallas Peters",
   description: "Product designer with over 13 years of design experience.",
-  url: "https://dallaspeters.co", // Domain of your site without prefix!
+  url: "dallaspeters.co", // Domain of your site without prefix!
   prefix: "/",
   author: "Dallas Peters",
   twitter: "Dallas Peters",
