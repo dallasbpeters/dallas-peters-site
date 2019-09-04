28409d97fcdea30200e1361a3f2674336953b37c
Dallas <dpeters@americanbible.org>
2019-09-04 16:46:13 -0500

    begin work on bio

--- src/pages/about.js
+++ src/pages/about.js
@@ -25,28 +25,25 @@ const AboutPage = ({ data }, location) => {
         ]}
       />
 
-      <article className="post-content page-template no-image">
-        <div className="post-content-body">
-          <h2>
-            I love to help people make things for the web. I've been doing this
-            for over ten years.
-          </h2>
-
-          <figure className="kg-card kg-image-card kg-width-full">
-            <Img
-              fluid={data.benchAccounting.childImageSharp.fluid}
-              className="kg-image"
-            />
-            <figcaption>The web is better with friends.</figcaption>
-          </figure>
-
-          <h3 id="dynamic-styles">Back On The Scene...</h3>
-          <p>
-            Being freshly back on the freelance scene its taking me just a
-            little while to get my site back in order but I promise to have it
-            soon!
-          </p>
+      <article className="post-content page-template">
+        <h2>Hi. I'm Dallas. I love to help people make things for the web.</h2>
+        <hr />
+        <div className="bio row">
+          <div className="col-4">
+            <figure className="bio--figure">
+              <Img fluid={data.dallasBioPhoto.childImageSharp.fluid} />
+            </figure>
+          </div>
+          <div className="col-8">
+            <h3>About Me</h3>
+            <p>
+              Being freshly back on the freelance scene its taking me just a
+              little while to get my site back in order but I promise to have it
+              soon!
+            </p>
+          </div>
         </div>
+        {/* end bio row */}
       </article>
     </Layout>
   )
@@ -59,11 +56,9 @@ const indexQuery = graphql`
         title
       }
     }
-    benchAccounting: file(
-      relativePath: { eq: "john-schnobrich-FlPc9_VocJ4-unsplash.jpg" }
-    ) {
+    dallasBioPhoto: file(relativePath: { eq: "dallas-profile.jpg" }) {
       childImageSharp {
-        fluid(maxWidth: 1360) {
+        fluid(maxWidth: 500) {
           ...GatsbyImageSharpFluid
         }
       }
